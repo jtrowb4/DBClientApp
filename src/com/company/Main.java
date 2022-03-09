3af22f7492c68358497a1516d90ac6fbb35597cf
jtrowb4 <jtrowb4@wgu.edu>
2022-03-08 20:58:57 -0600

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import helper.JDBC;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
+}
